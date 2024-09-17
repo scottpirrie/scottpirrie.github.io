206670d2b42fd39f8dc93d3dd8645eb50b9037fb
Scott <Scott.Pirrie2@xggc.scot.nhs.uk>
2024-09-17 17:04:19 +0100

    Basic routing and page navigation

---
+++ src/pages/Recipes.js
@@ -0,0 +1,14 @@
+//Home.js
+import React from 'react'
+import Navbar from './../components/Navbar'
+
+const Recipes = () => {
+    return (
+        <div>
+            <Navbar/>
+            <h1>Recipes</h1>
+        </div>
+    )
+}
+
+export default Recipes
